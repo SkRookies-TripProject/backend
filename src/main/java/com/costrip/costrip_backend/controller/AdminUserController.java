0841d52aa3202829cff2bda240ec016472726111
Hyein <hi020514@yourdomain.com>
2026-04-02 13:04:15 +0900

    관리자 권한 확인 추가

--- src/main/java/com/costrip/costrip_backend/controller/AdminUserController.java
+++ src/main/java/com/costrip/costrip_backend/controller/AdminUserController.java
@@ -6,6 +6,7 @@ import com.costrip.costrip_backend.entity.User;
 import com.costrip.costrip_backend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/admin/users")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminUserController {
 
     private final UserService userService;
